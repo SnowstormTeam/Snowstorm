2e01c8618b5577047cd3c2be9a37ff0042f5233c
D4V1D1997 <d4v1d1997@web.de>
2014-11-02 12:05:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Snowstorm [![Build Status](https://travis-ci.org/SnowstormTeam/Snowstorm.svg)](https://travis-ci.org/SnowstormTeam/Snowstorm)
+=========
+Snowstorm is a new modular Minecraft server software written in Java.
---
+++ src/main/java/org/snowstorm/Snowstorm.java
@@ -0,0 +1,11 @@
+package org.snowstorm;
+
+public class Snowstorm
+{
+	public static final String CORE_VERSION = "0.1-DEV";
+	
+	public static void main( String[] args )
+	{
+		System.out.println( "Starting Snowstorm " + CORE_VERSION + "..." );
+	}
+}
